32a6050e6bb626ffd5e49371d2994cbbe49970f9
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-05-15 11:21:20 +0900

    개발을 위한 초상태를 설정함
    핫 리로드를 위하여 게임모듈을 추가

--- Source/SpiderEditor.Target.cs
+++ Source/SpiderEditor.Target.cs
@@ -9,6 +9,6 @@ public class SpiderEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "Spider" } );
+		ExtraModuleNames.AddRange( new string[] { "Spider", "GameModule" } );
 	}
 }
